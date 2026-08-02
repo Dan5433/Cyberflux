a535e798645045dcf2724ab373ccd1d202503714
Dan5433 <grishkov.daniil@gmail.com>
2026-08-02 12:23:23 -0400

    Fix animations moving player

---
+++ Assets/Scripts/Player/LimbAnimation.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LimbAnimation : MonoBehaviour
+{
+    static readonly string ANIMATION_PARAMETER_IS_WALKING = "walking";
+    static readonly string ANIMATION_PARAMETER_IS_SPRINTING = "sprinting";
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void UpdateAnimations(Vector2 movementInput, bool isSprinting)
+    {
+        animator.SetBool(ANIMATION_PARAMETER_IS_WALKING, false);
+        animator.SetBool(ANIMATION_PARAMETER_IS_SPRINTING, false);
+
+        if (movementInput.magnitude <= 0)
+            return;
+
+
+        if (isSprinting)
+            animator.SetBool(ANIMATION_PARAMETER_IS_SPRINTING, true);
+        else
+            animator.SetBool(ANIMATION_PARAMETER_IS_WALKING, true);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,21 @@
 using InputSystem;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     static PlayerInput playerInput;
 
-    static readonly string ANIMATION_PARAMETER_IS_WALKING = "walking";
-    static readonly string ANIMATION_PARAMETER_IS_SPRINTING = "sprinting";
-
     [SerializeField] float speed = 5f;
     [SerializeField] float sprintSpeedMultiplier = 1.5f;
+    [SerializeField] LimbAnimation limbAnimation;
     bool isSprinting = false;
+    Vector2 movementInput;
 
     new Rigidbody rigidbody;
-    Animator animator;
 
     void Awake()
     {
-        animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
 
         playerInput = new();
@@ -39,30 +35,17 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 movementInput = playerInput.Player.Move.ReadValue<Vector2>();
-
         Vector3 velocity = new(movementInput.x, rigidbody.linearVelocity.y, movementInput.y);
         velocity *= speed;
         if (isSprinting)
             velocity *= sprintSpeedMultiplier;
 
         rigidbody.linearVelocity = velocity;
-
-        UpdateAnimations(movementInput);
     }
 
-    void UpdateAnimations(Vector2 movementInput)
+    void Update()
     {
-        animator.SetBool(ANIMATION_PARAMETER_IS_WALKING, false);
-        animator.SetBool(ANIMATION_PARAMETER_IS_SPRINTING, false);
-
-        if (movementInput.magnitude <= 0)
-            return;
-
-
-        if (isSprinting)
-            animator.SetBool(ANIMATION_PARAMETER_IS_SPRINTING, true);
-        else
-            animator.SetBool(ANIMATION_PARAMETER_IS_WALKING, true);
+        movementInput = playerInput.Player.Move.ReadValue<Vector2>();
+        limbAnimation.UpdateAnimations(movementInput, isSprinting);
     }
 }
