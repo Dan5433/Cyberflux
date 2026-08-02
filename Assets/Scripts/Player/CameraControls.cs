ef17142de940792eff90f16f69fcafa7decb32ab
Dan5433 <grishkov.daniil@gmail.com>
2026-08-02 17:05:29 -0400

    Add third person camera zoomout

--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraControls : MonoBehaviour
 {
-    [SerializeField] Transform playerRoot;
+    [SerializeField] Transform cameraPivot, playerRoot, head;
     [SerializeField] float mouseSensitivity = 0.5f;
+    [SerializeField] float zoomSensitivity = 0.25f;
     [SerializeField] Vector2 minMaxVerticalLook = new(-90f, 90f);
+    [SerializeField] float maxZoomOut = 10f;
     bool isMouseLocked = false;
     float zoom = 0f;
 
     void Update()
     {
+        UpdateZoom();
+        UpdateRotation();
+
+        if (zoom < 0)
+            transform.LookAt(head);
+        else
+            transform.localRotation = Quaternion.identity;
+    }
+
+    void UpdateRotation()
+    {
         Vector2 lookMovement = PlayerMovement.PlayerInput.Player.Look.ReadValue<Vector2>();
 
-        Vector3 cameraRotation = transform.rotation.eulerAngles;
         Vector3 rootRotation = playerRoot.rotation.eulerAngles;
+        Vector3 cameraRotation = cameraPivot.rotation.eulerAngles;
 
         if (cameraRotation.x > 180f)
             cameraRotation.x -= 360f;
         cameraRotation.x -= lookMovement.y * mouseSensitivity;
         cameraRotation.x = Mathf.Clamp(cameraRotation.x, minMaxVerticalLook.x, minMaxVerticalLook.y);
+
 
         if (ShouldRotateRoot())
             rootRotation.y += lookMovement.x * mouseSensitivity;
@@ -26,8 +40,19 @@ public class Camera : MonoBehaviour
             cameraRotation.y += lookMovement.x * mouseSensitivity;
 
 
-        transform.rotation = Quaternion.Euler(cameraRotation);
+        cameraPivot.rotation = Quaternion.Euler(cameraRotation);
         playerRoot.rotation = Quaternion.Euler(rootRotation);
+    }
+
+    void UpdateZoom()
+    {
+        Vector2 scrollInput = PlayerMovement.PlayerInput.Player.Zoom.ReadValue<Vector2>();
+
+        zoom += scrollInput.y * zoomSensitivity;
+        zoom = Mathf.Clamp(zoom, -maxZoomOut, 0);
+
+        Vector3 desiredPosition = cameraPivot.position - cameraPivot.forward * zoom;
+        transform.position = desiredPosition;
     }
 
     bool ShouldRotateRoot()
