10e6b2b03ff8c0c61cee7149f5cd0e011f78a880
Dan5433 <grishkov.daniil@gmail.com>
2026-08-03 14:16:02 -0400

    Use Cinemachine 3rd person follow

--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -1,24 +1,21 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
-    [SerializeField] Transform cameraPivot, playerRoot, head;
+    [SerializeField] CinemachineThirdPersonFollow thirdPersonFollow;
+    [SerializeField] Transform playerRoot;
     [SerializeField] float mouseSensitivity = 0.5f;
     [SerializeField] float zoomSensitivity = 0.25f;
     [SerializeField] Vector2 minMaxVerticalLook = new(-90f, 90f);
-    [SerializeField] float maxZoomOut = 10f;
+    [SerializeField] float maxCameraDistance = 10f;
     bool isMouseLocked = false;
-    float zoomOut = 0f;
-
-    public Transform CameraPivot => cameraPivot;
+    float cameraDistance = 0f;
 
     void Update()
     {
         UpdateZoom();
         UpdateRotation();
-
-        if (zoomOut < 0)
-            transform.LookAt(head);
     }
 
     void UpdateRotation()
@@ -26,7 +23,7 @@ public class CameraControls : MonoBehaviour
         Vector2 lookMovement = PlayerMovement.PlayerInput.Player.Look.ReadValue<Vector2>();
 
         Vector3 rootRotation = playerRoot.rotation.eulerAngles;
-        Vector3 cameraRotation = cameraPivot.localRotation.eulerAngles;
+        Vector3 cameraRotation = transform.localRotation.eulerAngles;
 
         if (cameraRotation.x > 180f)
             cameraRotation.x -= 360f;
@@ -39,27 +36,25 @@ public class CameraControls : MonoBehaviour
             rootRotation.y += lookMovement.x * mouseSensitivity;
             cameraRotation.y = 0;
         }
-        else
-            cameraRotation.y += lookMovement.x * mouseSensitivity;
+        cameraRotation.y += lookMovement.x * mouseSensitivity;
 
 
         playerRoot.rotation = Quaternion.Euler(rootRotation);
-        cameraPivot.localRotation = Quaternion.Euler(cameraRotation);
+        transform.localRotation = Quaternion.Euler(cameraRotation);
     }
 
     void UpdateZoom()
     {
         Vector2 scrollInput = PlayerMovement.PlayerInput.Player.Zoom.ReadValue<Vector2>();
 
-        zoomOut -= scrollInput.y * zoomSensitivity;
-        zoomOut = Mathf.Clamp(zoomOut, 0, maxZoomOut);
+        cameraDistance -= scrollInput.y * zoomSensitivity;
+        cameraDistance = Mathf.Clamp(cameraDistance, 0, maxCameraDistance);
 
-        Vector3 desiredPosition = cameraPivot.position - cameraPivot.forward * zoomOut;
-        transform.position = desiredPosition;
+        thirdPersonFollow.CameraDistance = cameraDistance;
     }
 
     bool ShouldRotateRoot()
     {
-        return isMouseLocked || zoomOut == 0;
+        return isMouseLocked || cameraDistance == 0;
     }
 }
